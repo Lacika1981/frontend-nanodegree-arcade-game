e829f6fe290b934b2810ee3e324ca0f8f88ec55c
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-06-21 10:19:09 +0100

    working on the enemies

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var Enemy = function (speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = -1; //the enemy will start off screen
+    this.x = -110; //the enemy will start off screen
     this.y = this.startPosition();
     console.log(this.y);
     this.speed = speed;
@@ -18,7 +18,8 @@ Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + (this.x * dt);
+    //this.x = this.x + (this.x * dt);
+    this.x++;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -78,14 +79,14 @@ document.addEventListener('keyup', function (e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    //player.handleInput(allowedKeys[e.keyCode]);
 });
 
 
 // function to generate the y position for the bug to start
 
 Enemy.prototype.startPosition = function () {
-    var bugPos = [3, 4, 5];
+    var bugPos = [81, 162, 243];
     var randomPos = bugPos[Math.floor(Math.random() * bugPos.length)];
     return randomPos;
 };
