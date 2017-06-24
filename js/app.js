489b4ac2269c288f96d1955ee617a62740919347
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-06-24 20:18:14 +0100

    changing the sizes of the canvas and updating the app.js file

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ Enemy.prototype.update = function (dt) {
         this.x = this.startPosX();
         this.y = this.startPosY();
     }
-    this.x++;
+    this.x = this.x + (this.speed * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,14 +36,13 @@ Enemy.prototype.render = function () {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function (x, y) {
-    this.x = x;
-    this.y = y;
-    this.sprite = "char-boy.png";
+var Player = function () {
+    this.x = 200;
+    this.y = 200;
+    this.sprite = 'images/char-boy.png';
 }
 
 Player.prototype.update = function () {
-
 }
 
 Player.prototype.handleInput = function (keyEvent) {
@@ -67,13 +66,16 @@ Player.prototype.handleInput = function (keyEvent) {
 
 }
 
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 var allEnemies = [];
-console.log(allEnemies);
-var player = {};
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -87,11 +89,18 @@ document.addEventListener('keyup', function (e) {
 
     //player.handleInput(allowedKeys[e.keyCode]);
 });
-var bugPosX = [-125];
+var bugPosX = -125;
 
 // function to generate the y position for the bug to start
 Enemy.prototype.startPosX = function () {
-    var randomPosX = bugPosX[Math.floor(Math.random() * bugPosX.length)];
+    if (bugPosX < -1025) {
+            bugPosX = -125;
+        } else {
+            var diffX = [0, 150, 300, 450, 600];
+            var randomPosX = diffX[Math.floor(Math.random() * diffX.length)];
+            bugPosX -= randomPosX;
+        }
+    var randomPosX = bugPosX;
     return randomPosX;
 };
 
@@ -103,16 +112,16 @@ Enemy.prototype.startPosY = function () {
 
 function numberOfEnemies(num) {
     for (var i = 0; i < num; i++) {
-        allEnemies.push(new Enemy());
-        if (bugPosX[0] < -125) {
-            bugPosX[0] = -125;
+        allEnemies.push(new Enemy(125));
+        if (bugPosX < -325) {
+            bugPosX = -125;
         } else {
-            bugPosX[0] -= 125;
+            bugPosX -= 200;
         }
     }
 }
 
-numberOfEnemies(3);
+numberOfEnemies(5);
 
 /*var enemyOne = new Enemy(5, -200);
 allEnemies.push(enemyOne);
